d11481da04d652b7e903bfb90961a84585d0b679
Maria Fernanda Muzetti Ribeiro <mfmuzi@gmail.com>
2021-12-13 13:42:03 -0300

    Adição da página de erro

--- README.md
+++ README.md
@@ -1,6 +1,52 @@
-## URL Shortener
+# URL Shortener
 
-Projeto desenvolvido baseado na aula apresentada pela Digital Innovation One. Foi desenvolvido um encurtador de url a partir da construção de uma API com NodeJS, Typescript e MongoDB para a base de dados.
+Este projeto teve como objetivo criar um encurtador de URL com NodeJS, Typescript e MongoDB para a base de dados.
 
-Utilizei o supervisor no lugar do nodemon para conexão com o banco de dados e fiz o redirecionamento do erro 400 para uma página de erro personalizada da Pixar.
+Utilizei o supervisor no lugar do nodemon para conexão com o banco de dados e fiz o redirecionamento do erro 400 para uma página personalizada.
 
+
+
+## Rodando o projeto
+
+Na pasta raiz digite os comandos:
+
+- `npm install`
+- `npm run build:watch`
+- `npm run dev`
+
+
+
+### Conexão om o MongoDB:
+
+Substituir os parâmetros no arquivo Constants.ts:
+
+```
+mongodb+srv://<user>:<password>@<database>.q5vfq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+```
+
+- Substitua <user> pelo nome do usuário do banco de dados.
+
+- Substitua <password> pela senha do banco de dados.
+
+- Substitua <database> pelo nome do banco de dados.
+
+- Substitua myFirstDatabase pelo nome do banco de dados.
+
+
+
+### Executando o encurtador
+
+Após a conexão com o MongoDB:
+
+- realizar o post da URL a ser encurtada pelo Postman ou Insomnia:
+
+```
+http://localhost:5000/shorten
+
+com o corpo:
+{
+"originURL":"URL para encurtar"
+}
+```
+
+Como saída temos a URL encurtada.
--- url-shortener/dist/controller/URLController.js
+++ url-shortener/dist/controller/URLController.js
@@ -13,8 +13,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.URLController = void 0;
-const Constants_1 = require("../config/Constants");
 const shortid_1 = __importDefault(require("shortid"));
+const Constants_1 = require("../config/Constants");
 const URL_1 = require("../database/model/URL");
 class URLController {
     shorten(req, res) {
@@ -39,8 +39,7 @@ class URLController {
                 res.redirect(url.originURL);
                 return;
             }
-            res.status(400).redirect('https://www.pixar.com/404');
-            res.redirect(url.originURL);
+            res.status(400).send('<!DOCTYPE html><html lang="en"><head><style type="text/css">body {display: flex;flex-flow: row wrap;align-content: center;justify-content: center;}div {width: 100%;text-align: center;}.number {background: #fff;position: relative;font: 900 30vmin "Consolas";letter-spacing: 5vmin;text-shadow: 2px -1px 0 #000, 4px -2px 0 #0a0a0a, 6px -3px 0 #0f0f0f, 8px -4px 0 #141414, 10px -5px 0 #1a1a1a, 12px -6px 0 #1f1f1f, 14px -7px 0 #242424, 16px -8px 0 #292929;}.number::before {background-color: #673ab7;background-image: radial-gradient(closest-side at 50% 50%, #ffc107 100%, rgba(0, 0, 0, 0)), radial-gradient(closest-side at 50% 50%, #e91e63 100%, rgba(0, 0, 0, 0));background-repeat: repeat-x;background-size: 40vmin 40vmin;background-position: -100vmin 20vmin, 100vmin -25vmin;width: 100%;height: 100%;mix-blend-mode: screen;-webkit-animation: moving 10s linear infinite both;animation: moving 10s linear infinite both;display: block;position: absolute;content: "";}@-webkit-keyframes moving {to {background-position: 100vmin 20vmin, -100vmin -25vmin;}}@keyframes moving {to {background-position: 100vmin 20vmin, -100vmin -25vmin;}}.text {font: 400 5vmin "Courgette";color:#673ab7;}.text span {font-size: 10vmin;color: #5387b3;}</style><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="./index.css"><title>Document</title></head><body><div class="number">400</div><div class="text"><span>Ooops...</span><br><br>A URL informada está incorreta.</div></body></html>');
         });
     }
 }
